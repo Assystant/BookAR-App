f9c06162cfe232e638beab5b1980614318392e4f
Assystant <contact@travelder.com>
2021-04-26 16:40:45 +0530

    Updated pod list Update auth navigation Added script commands

--- App.tsx
+++ App.tsx
@@ -6,15 +6,13 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
+import React, {useEffect, createContext, useMemo} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 // import { Provider } from 'react-redux';
 
 // import { createStore, applyMiddleware, StoreEnhancer } from 'redux';
-
 
 // import BookList from './app/screens/bookList';
 import LogIn from './app/screens/login';
@@ -24,32 +22,127 @@ import BookDescription from './app/screens/bookDescription';
 import BookDEtails from './app/screens/bookDetails';
 import BookCard from './app/screens/bookCard';
 import CalenderExample from './app/screens/calender';
+import {AsyncStorage} from 'react-native';
+
+const AuthContext = createContext<any>(null);
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    },
+  );
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
 
+      try {
+        userToken = await AsyncStorage.getItemAsync('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({type: 'RESTORE_TOKEN', token: userToken});
+    };
+
+    bootstrapAsync();
+  }, []);
+  const authContext = useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `SecureStore`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+      signOut: () => dispatch({type: 'SIGN_OUT'}),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `SecureStore`
+        // In the example, we'll use a dummy token
+
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
+      },
+    }),
+    [],
+  );
+
+  // if (state.isLoading) {
+  //   // We haven't finished checking for the token yet
+  //   return <SplashScreen />;
+  // }
   return (
-
     // <BookList />
     // <Provider >
     // <LogIn username={null} password={null}/>
     // </Provider>
     // <DownloadFile isDone={false}/>
     // <BookDescription/>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={LogIn} />
-        <Stack.Screen name="BookList" component={BookList} />
-        <Stack.Screen name="BookPhraseDetails" component={BookDescription}/>
-        <Stack.Screen name="BookDetails" component={BookDEtails}/>
-        <Stack.Screen name="BookCard" component={BookCard}/>
-        <Stack.Screen name="CalenderExample" component={CalenderExample}/>
-
-        
-      </Stack.Navigator>
-    </NavigationContainer>
-
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {state.userToken == null ? (
+            <>
+              <Stack.Screen
+                name="Home"
+                component={LogIn}
+                options={{
+                  animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+                }}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="BookList" component={BookList} />
+              <Stack.Screen
+                name="BookPhraseDetails"
+                component={BookDescription}
+              />
+              <Stack.Screen name="BookDetails" component={BookDEtails} />
+              <Stack.Screen name="BookCard" component={BookCard} />
+              <Stack.Screen
+                name="CalenderExample"
+                component={CalenderExample}
+              />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 };
 
